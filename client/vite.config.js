78252769eecd4d57b578a8d0bb6bd04c53b2d9a8
sidhu1819 <832SIDDHARTHA@GMAIL.COM>
2026-04-14 02:56:57 +0530

    fix vite build crash 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
+    },
   },
+
   optimizeDeps: {
     exclude: ['three']
   }
